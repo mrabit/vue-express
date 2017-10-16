fc94553f4ba4737342bf05329ccb70bc4cc6ff8e
一桶浆糊 <biabia123456@126.com>
2017-10-16 14:24:28 +0800

    remove the console.log

--- src/server/router.js
+++ src/server/router.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    console.log(req);
+    
     if (process.env.NODE_ENV == 'development') {
         res.render('index_dev', { title: 'Express' });
         return;
